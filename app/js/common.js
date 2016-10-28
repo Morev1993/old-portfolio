aa82f7d9b0aa699492b11948c466e826351da827
Игорь <igor@MacBook-Pro-Igor.local>
2016-10-28 23:01:54 +1000

    start development

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,6 @@
 $(document).ready(() => {
-	$('.more').click(() => {
-		$('.bg').removeClass('showed');
-		$('header').removeClass('slide-right');
-		$('.main-page').addClass('slide-left');
-		$('.main-page .content').addClass('fade-hide');
-		$('.main-page').removeClass('in');
-		$('.main-page').addClass('out');
-	});
-
-	$('.bg').addClass('showed');
-	$('header').addClass('slide-right');
-
-	$('.typist')
-		.typist({ speed: 15 })
-		.typistPause(400)
-		.typistAdd('Услуги от фронтенд-разработчика')
-		.on('end_type.typist', function() {
-        	$('.more').addClass('animated jello showed');
-        	$('.main-page').addClass('in');
-    	})
+	$(document).click(()=> {
+    $('.bg').toggleClass('js-full');
+  })
 
 });
