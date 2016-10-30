0d91f5dde6b67ec139022b402ef3a82e47c63f0f
Игорь <igor@MacBook-Pro-Igor.local>
2016-10-30 21:16:47 +1000

    fix style bugs, add tabs

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
+	$('.project').toggleClass('js-slide-left');
+
 	$(document).click(()=> {
-    $('.bg').toggleClass('js-full');
-  })
+    	$('.project').addClass('js-full');
+		$('.arrow').fadeOut();
+  	})
+
+	$(".tab-item").not(":first").hide();
+	$(".tab").click(function() {
+		$(".tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab-item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
 
 });
