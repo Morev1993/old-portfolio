c02d01b5c4a32fb021f257f5f24e4762de885d81
Игорь <igor@MacBook-Pro-Igor.local>
2016-11-07 15:38:42 +1000

    update project features

--- app/js/common.js
+++ app/js/common.js
@@ -6,49 +6,105 @@ $(document).ready(() => {
 	var projectTmpl = Handlebars.compile($('#project').html());
 	var infoTmpl = Handlebars.compile($('#info').html());
 
+	function checkState(context, next){
+        updateMenuItems(context.path);
+		updateDotsItems(context.path);
+    }
+
+    function updateMenuItems(path) {
+        var nav = $('.navigation li');
+        nav.removeClass('active');
+        nav.each(function(i, item) {
+            var href = $(item).find('a').attr('href');
+            if(~path.indexOf(href)) {
+                $(item).addClass('active js-stroke');
+            }
+        })
+    }
+
+	function updateDotsItems(path) {
+        var nav = $('.dots a');
+        nav.removeClass('active');
+        nav.each(function(i, item) {
+            var href = $(item).attr('href');
+            if(~path.indexOf(href)) {
+                $(item).addClass('active');
+            }
+        })
+    }
+
 
 	$body.on('click', '#show-detail', function() {
+		$('.bg-overlay').removeClass('light');
 		$project.addClass('js-full');
-		$('.arrow').fadeOut();
+		$('.controls').fadeOut();
+		$('.project-nav').addClass('visible');
+		$(this).hide();
+	});
+
+	$body.on('click', '#js-back', function() {
+		$project.removeClass('js-full');
+		$('.controls').fadeIn();
+		$('.project-nav').removeClass('visible');
+		$('#show-detail').fadeIn();
+		$('.bg-overlay').addClass('light');
+	});
+
+	$body.on('click', '.arrow-right', function(e) {
+		var e = e;
+		e.preventDefault();
+		$('.tab-content').addClass('hidy');
+		$('.bg-overlay').removeClass('light');
+
+		TweenLite.to(".project", 1, {className:"-=js-slide-left", onComplete: function() {
+			page(e.target.pathname);
+		}});
+	});
+
+	$body.on('click', '.arrow-left', function(e) {
+		var e = e;
+		e.preventDefault();
+		$('.tab-content').addClass('hidy');
+		$('.bg-overlay').removeClass('light');
+
+		TweenLite.to(".project", 1, {className:"-=js-slide-left", onComplete: function() {
+			page(e.target.pathname);
+		}});
 	});
 
 	function setTabs() {
 		$(".tab-item").not(":first").hide();
 		$(".tab").click(function(e) {
 			e.preventDefault();
-			$project.addClass('js-full');
-			$(".tab").removeClass("active").eq($(this).index()).addClass("active");
-			$(".tab-item").hide().eq($(this).index()).show()
+			$(".tab").removeClass("active").eq($(this).index() - 1).addClass("active");
+			$(".tab-item").hide().eq($(this).index() -1).show()
 		}).eq(0).addClass("active");
 	}
 
-	function pageIntro(id) {
-		$project.removeClass('js-slide-left');
-		$.get( "http://localhost/personal/wp-json/wp/v2/posts?filter[name]=" + id + "", function( data ) {
+	function pageIntro(ctx, next) {
+		$.get( "http://localhost/personal/wp-json/wp/v2/posts?filter[name]=" + ctx.params.id + "", function( data ) {
 			if (data.length) {
 				$projectBlock.html(projectTmpl(data[0]));
-				$('.tab-item').addClass('hidy');
-				setTimeout(function() {
-					$('.bg-overlay').addClass('light');
-					$project.addClass('js-slide-left');
-					$project.removeClass('js-slide-right');
-					$project.removeClass('js-full');
-					$('.tab-item').removeClass('hidy');;
-				}, 200)
+
+				TweenLite.to($project, 1, {className:"+=js-slide-left"});
+
+				$('.bg-overlay').addClass('light');
 				setTabs();
 			} else {
 				$projectBlock.html(404);
 			}
+
+			next();
 		});
 	}
 
-	function info() {
-		$project.removeClass('js-slide-left').removeClass('js-full');
-		$('.tab-item').addClass('hidy');
+	function info(ctx, next) {
+		$('.tab-content').addClass('hidy');
 		$('.bg-overlay').removeClass('light');
-		setTimeout(function() {
-			$projectBlock.html(infoTmpl());
-		}, 1100);
+
+		$projectBlock.html(infoTmpl());
+
+		next();
 	}
 
 	function notFound() {
@@ -59,35 +115,13 @@ $(document).ready(() => {
 		page.redirect('/projects/axios-engeeniring/');
 	}
 
-	$body.on('click', '.arrow-right', function(e) {
-		e.preventDefault();
-		$('.tab-item').addClass('hidy');
-		$('.bg-overlay').removeClass('light');
-		$project.removeClass('js-slide-left');
-		setTimeout(function() {
-			page(e.target.pathname);
-		}, 1100);
-	});
+	page('/', redirectToIntro, checkState)
+	page('/projects', redirectToIntro, checkState)
+	page('/projects/:id', pageIntro, checkState);
 
-	$body.on('click', '.arrow-left', function(e) {
-		e.preventDefault();
-		$('.tab-item').addClass('hidy');
-		$('.bg-overlay').removeClass('light');
-		$project.addClass('js-slide-right');
-		setTimeout(function() {
-			page(e.target.pathname);
-		}, 1100);
-	});
+	page('/info', info, checkState)
 
-	page('/', redirectToIntro)
-	page('/projects', redirectToIntro)
-	page('/projects/:id', function(ctx) {
-		pageIntro(ctx.params.id);
-	});
-
-	page('/info', info)
-
-	page('*', notFound)
+	page('*', notFound, checkState)
 	page()
 
 });
