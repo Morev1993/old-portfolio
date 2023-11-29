5a684db5259511ccab734748a291e62185644165
Igor Morev <morevin@pik.ru>
2023-11-29 18:24:40 +1000

    reworked project

--- app/js/common.js
+++ app/js/common.js
@@ -1,158 +1,164 @@
-$(document).ready(function() {
-	var $projectBlock = $('#project-block');
-	var $project = $('.project');
-	$body = $('html, body');
+$(document).ready(function () {
+  var $projectBlock = $("#project-block");
+  var $project = $(".project");
+  $body = $("html, body");
 
-	var projectTmpl = Handlebars.compile($('#project').html());
-	var infoTmpl = Handlebars.compile($('#info').html());
+  var projectTmpl = Handlebars.compile($("#project").html());
+  var infoTmpl = Handlebars.compile($("#info").html());
 
-	function checkState(context, next){
-        updateMenuItems(context.path);
-		updateDotsItems(context.path);
+  function checkState(context, next) {
+    updateMenuItems(context.path);
+    updateDotsItems(context.path);
+  }
+
+  function updateMenuItems(path) {
+    var nav = $(".navigation li");
+    nav.removeClass("active");
+    nav.each(function (i, item) {
+      var href = $(item).find("a").attr("href");
+      if (~path.indexOf(href)) {
+        $(item).addClass("active js-stroke");
+      }
+    });
+  }
+
+  function updateDotsItems(path) {
+    var nav = $(".dots a");
+    nav.removeClass("active");
+    nav.each(function (i, item) {
+      var href = $(item).attr("href");
+      if (~path.indexOf(href)) {
+        $(item).addClass("active");
+      }
+    });
+  }
+
+  function hideDetail() {
+    $project.removeClass("js-full");
+    $(".project-nav").removeClass("visible");
+    $("#show-detail").addClass("visible");
+    $(".bg-overlay").addClass("light");
+    $(".preview").removeClass("js-detail");
+    $(".tab.about").click();
+    $(".controls").addClass("js-showed");
+
+    updateMenuItems(location.pathname);
+  }
+
+  function showDetail() {
+    $(".bg-overlay").removeClass("light");
+    $project.addClass("js-full");
+    $(".project-nav").addClass("visible");
+    $(".preview").addClass("js-detail");
+    $(this).removeClass("visible");
+    $(".controls").removeClass("js-showed");
+
+    var nav = $(".navigation li");
+    nav.removeClass("active");
+  }
+
+  $body.on("click", ".shadow", showDetail);
+
+  $body.on("click", "#js-back", hideDetail);
+
+  $(".navigation li a").on("click", function (e) {
+    var e = e;
+    e.preventDefault();
+
+    var href = e.target.pathname;
+    if (~location.pathname.indexOf("/projects") && href == "/projects") {
+      hideDetail();
+      return;
     }
 
-    function updateMenuItems(path) {
-        var nav = $('.navigation li');
-        nav.removeClass('active');
-        nav.each(function(i, item) {
-            var href = $(item).find('a').attr('href');
-            if(~path.indexOf(href)) {
-                $(item).addClass('active js-stroke');
-            }
-        });
+    if (~location.pathname.indexOf("/info") && href == "/projects") {
+      TweenLite.to(".project", 1, { className: "-=js-slide-left" });
+      page(href);
+      return;
     }
 
-	function updateDotsItems(path) {
-        var nav = $('.dots a');
-        nav.removeClass('active');
-        nav.each(function(i, item) {
-            var href = $(item).attr('href');
-            if(~path.indexOf(href)) {
-                $(item).addClass('active');
-            }
-        })
-    }
+    hideDetail();
+    page(href);
+  });
 
-	function hideDetail( ) {
-		$project.removeClass('js-full');
-		$('.project-nav').removeClass('visible');
-		$('#show-detail').addClass('visible');
-		$('.bg-overlay').addClass('light');
-		$('.preview').removeClass('js-detail');
-		$(".tab.about").click();
-		$('.controls').addClass('js-showed');
+  function triggerSlide(e) {
+    var e = e;
+    e.preventDefault();
 
-		updateMenuItems(location.pathname);
-	}
+    var pathname = $(this).attr("href");
 
-	function showDetail() {
-		$('.bg-overlay').removeClass('light');
-		$project.addClass('js-full');
-		$('.project-nav').addClass('visible');
-		$('.preview').addClass('js-detail');
-		$(this).removeClass('visible');
-		$('.controls').removeClass('js-showed');
+    $(".tab-content").addClass("hidy");
+    $(".bg-overlay").removeClass("light");
+    $(".controls").removeClass("js-showed");
 
-		var nav = $('.navigation li');
-        nav.removeClass('active');
-	}
+    TweenLite.to(".project", 1, {
+      className: "-=js-slide-left",
+      onComplete: function () {
+        page(pathname);
+      },
+    });
+  }
 
+  $body.on("click", ".controls a", triggerSlide);
 
-	$body.on('click', '#show-detail', showDetail);
+  function setTabs() {
+    $(".tab-item:first").addClass("visible");
+    $(".tab")
+      .click(function (e) {
+        e.preventDefault();
+        $(".tab")
+          .removeClass("active")
+          .eq($(this).index() - 1)
+          .addClass("active");
+        $(".tab-item")
+          .removeClass("visible")
+          .eq($(this).index() - 1)
+          .addClass("visible");
+      })
+      .eq(0)
+      .addClass("active");
+  }
 
-	$body.on('click', '#js-back', hideDetail);
+  function pageIntro(ctx, next) {
+    $projectBlock.html(projectTmpl({}));
 
-	$('.navigation li a').on('click', function(e) {
-		var e = e;
-		e.preventDefault();
+    $(".bg-overlay").addClass("light");
+    setTabs();
 
-		var href = e.target.pathname;
-		if (~location.pathname.indexOf('/projects') && href == '/projects') {
-			hideDetail();
-			return;
-		};
+    TweenLite.to($project, 0.6, {
+      className: "+=js-slide-left",
+      onComplete: function () {
+        $(".controls").addClass("js-showed");
+        $("#show-detail").addClass("visible");
+        next();
+      },
+    });
+  }
 
-		if (~location.pathname.indexOf('/info') && href == '/projects') {
-			TweenLite.to(".project", 1, {className:"-=js-slide-left"});
-			page(href);
-			return;
-		};
+  function info(ctx, next) {
+    $(".tab-content").addClass("hidy");
+    $(".bg-overlay").removeClass("light");
+    $("#show-detail").hide();
 
-		hideDetail();
-		page(href);
-	})
+    $projectBlock.html(infoTmpl());
 
-	function triggerSlide(e) {
-		var e = e;
-		e.preventDefault();
+    next();
+  }
 
-		var pathname = $(this).attr('href');
+  function notFound() {
+    $projectBlock.html(404);
+  }
 
-		$('.tab-content').addClass('hidy');
-		$('.bg-overlay').removeClass('light');
-		$('.controls').removeClass('js-showed');
+  function redirectToIntro() {
+    page.redirect("/projects/axios-engeeniring/");
+  }
 
-		TweenLite.to(".project", 1, {className:"-=js-slide-left", onComplete: function() {
-			page(pathname);
-		}});
-	}
+  page("/", redirectToIntro, checkState);
+  page("/projects", redirectToIntro, checkState);
+  page("/projects/:id", pageIntro, checkState);
 
-	$body.on('click', '.controls a', triggerSlide);
+  page("/info", info, checkState);
 
-	function setTabs() {
-		$(".tab-item:first").addClass('visible');
-		$(".tab").click(function(e) {
-			e.preventDefault();
-			$(".tab").removeClass("active").eq($(this).index() - 1).addClass("active");
-			$(".tab-item").removeClass('visible').eq($(this).index() -1).addClass('visible')
-		}).eq(0).addClass("active");
-	}
-
-	function pageIntro(ctx, next) {
-		$.get( "http://igor-morev.ru/personal/wp-json/wp/v2/posts?filter[name]=" + ctx.params.id + "", function( data ) {
-			if (data.length) {
-				$projectBlock.html(projectTmpl(data[0]));
-
-				$('.bg-overlay').addClass('light');
-				setTabs();
-
-				TweenLite.to($project, .6, {className:"+=js-slide-left", onComplete: function() {
-					$('.controls').addClass('js-showed');
-					$('#show-detail').addClass('visible');
-					next();
-				}});
-			} else {
-				$projectBlock.html(404);
-				next();
-			}
-		});
-	}
-
-	function info(ctx, next) {
-		$('.tab-content').addClass('hidy');
-		$('.bg-overlay').removeClass('light');
-		$('#show-detail').hide();
-
-		$projectBlock.html(infoTmpl());
-
-		next();
-	}
-
-	function notFound() {
-		$projectBlock.html(404);
-	}
-
-	function redirectToIntro() {
-		page.redirect('/projects/axios-engeeniring/');
-	}
-
-	page('/', redirectToIntro, checkState)
-	page('/projects', redirectToIntro, checkState)
-	page('/projects/:id', pageIntro, checkState);
-
-	page('/info', info, checkState)
-
-	page('*', notFound, checkState)
-	page()
-
+  page("*", notFound, checkState);
+  page();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,49 @@
-'use-strict'
+"use-strict";
 
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    livereload = require('gulp-livereload'),
-    watch = require('gulp-watch'),
-    autoprefixer = require('gulp-autoprefixer'),
-    connect = require('gulp-connect');
-    pug = require('gulp-pug');
-    historyApiFallback = require('connect-history-api-fallback');
+var gulp = require("gulp"),
+  less = require("gulp-less"),
+  livereload = require("gulp-livereload"),
+  watch = require("gulp-watch"),
+  autoprefixer = require("gulp-autoprefixer"),
+  connect = require("gulp-connect");
+historyApiFallback = require("connect-history-api-fallback");
 
 //server
-gulp.task('server', function() {
-
-    connect.server({
-      root: 'app',
-      port: 8888,
-      livereload: true,
-      middleware: function(connect, opt) {
-          return [ historyApiFallback() ];
-        }
-    });
-
+gulp.task("server", function () {
+  connect.server({
+    root: "app",
+    port: 8888,
+    livereload: true,
+    middleware: function (connect, opt) {
+      return [historyApiFallback()];
+    },
+  });
 });
 
 //html
-gulp.task('html', function() {
-  gulp.src('./app/*.html')
-  .pipe(connect.reload());
+gulp.task("html", function () {
+  gulp.src("./app/*.html").pipe(connect.reload());
 });
 
 //less
-gulp.task('less', function() {
-    gulp.src('./app/less/main.less')
-        .pipe(less())
-        .pipe(autoprefixer({
-            browsers: ['last 15 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./app/css'))
-        .pipe(connect.reload());
+gulp.task("less", function () {
+  gulp
+    .src("./app/less/main.less")
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 15 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./app/css"))
+    .pipe(connect.reload());
 });
-
-
-gulp.task('pug', function(){
-    gulp.src('./app/pug/*.pug')
-        .pipe(pug({
-            pretty: true
-        })).on("error", console.log)
-        .pipe(gulp.dest('./app'))
-        .pipe(connect.reload());
-});
-
 
 //watch
-gulp.task('watch', function() {
-    gulp.watch('./app/less/*.less', ['less']);
-    gulp.watch('./app/pug/*.pug', ['pug']);
-    gulp.watch('./app/pug/inc/*.pug', ['pug']);
-    gulp.watch('./app/pug/mixins/*.pug', ['pug']);
+gulp.task("watch", function () {
+  gulp.watch("./app/less/*.less", ["less"]);
 });
 
 //default
-gulp.task('default', ['server', 'html', 'less', 'watch']);
+gulp.task("default", ["server", "html", "less", "watch"]);
